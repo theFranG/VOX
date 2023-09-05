1816b97f9b03709e4a73a2840b304528dbe20db4
Franco <francogomez23800@gmail.com>
2023-09-05 15:39:49 -0300

    env

--- app/(root)/communities/page.tsx
+++ app/(root)/communities/page.tsx
@@ -1,4 +1,12 @@
-const Page = () => {
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { fetchUser } from "@/lib/actions/user.actions";
+import { fetchCommunities } from "@/lib/actions/community.actions";
+
+const Page = ({
+     searchParams
+     }:{
+     searchParams:{[key:string]:string | undefined}}) => {
     return (
      <section>
           <h1 className="head-text mb-10">Communities</h1>
